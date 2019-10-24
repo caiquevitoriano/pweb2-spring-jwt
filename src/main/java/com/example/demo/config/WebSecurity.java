e53320ccc5ae75b45ede4e9db7478bd4cdd4f435
caiquevitoriano <vitorianocaique@gmail.com>
2019-10-23 21:28:39 -0300

    postman

--- src/main/java/com/example/demo/config/WebSecurity.java
+++ src/main/java/com/example/demo/config/WebSecurity.java
@@ -38,8 +38,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                    .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                    .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
